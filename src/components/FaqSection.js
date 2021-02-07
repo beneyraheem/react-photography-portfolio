7617cbc2f6a1a258f45da5474f2724ed26d611e3
Beney Raheem <beneyx@gmail.com>
2021-02-07 13:56:08 +0500

    add sections

---
+++ src/components/FaqSection.js
@@ -0,0 +1,66 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            quaerat.
+          </p>
+        </div>
+      </div>
+      {/* break */}
+      <div className="question">
+        <h4>How do I end?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            quaerat.
+          </p>
+        </div>
+      </div>
+      {/* break */}
+      <div className="question">
+        <h4>How do I new?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            quaerat.
+          </p>
+        </div>
+      </div>
+      {/* break */}
+      <div className="question">
+        <h4>How do I frequent?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            quaerat.
+          </p>
+        </div>
+      </div>
+      {/* break */}
+      <div className="question">
+        <h4>How do I something?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            quaerat.
+          </p>
+        </div>
+      </div>
+      {/* break */}
+    </div>
+  );
+};
+
+export default FaqSection;
